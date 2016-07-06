794860eca15c1b632239e307f709e8af939ce600
mikeanan <chennanbang@gmail.com>
2016-07-06 15:34:28 +0800

    子類別增加竉物名稱屬性

--- Extends.php
+++ Extends.php
@@ -17,13 +17,28 @@ class Animal
 
 class Cat extends Animal
 {
-    public function __construct()
+    private $petName;
+
+    public function __construct($name)
     {
         parent::__construct("貓");//呼叫父類別時，使用 parent::   用來設定物種名稱，一次性，不會改變
+        $this->petName = $name;//設定竉物名稱
+    }
+
+    public function getPetName()
+    {
+        return $this->petName;
+    }
+
+    public function setPetName($petName)//竉物名稱是可以改變的
+    {
+        $this->petName = $petName;
     }
 }
 
-$petCat = new Cat();//產生貓別，初始化時設定物種名稱
+$petCat = new Cat("Kitty");//產生貓別，初始化時設定物種名稱。傳入竉物名稱。
 echo $petCat->getAnimalType();
+echo "<br>";
+echo $petCat->getPetName();
 
 echo "<br>END<br>";
